10c7834d94956760a0813909fb79e910b7469b9e
ingvord <igor.khokhriakov@rcsb.org>
2023-03-06 12:03:47 -0800

    Progress

--- src/main/java/com/github/ingvord/axsis/AxsisObserver.java
+++ src/main/java/com/github/ingvord/axsis/AxsisObserver.java
@@ -5,7 +5,8 @@ import co.elastic.apm.api.Span;
 import co.elastic.apm.api.Transaction;
 import co.elastic.apm.attach.ElasticApmAttacher;
 import com.github.ingvord.axsis.message.AxsisMoveMessage;
-import com.github.ingvord.axsis.message.AxsisDoneMessage;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.observables.GroupedObservable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import io.reactivex.rxjava3.subjects.PublishSubject;
 import magix.SseMagixClient;
@@ -13,11 +14,18 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.plugins.providers.jackson.ResteasyJackson2Provider;
 
 import javax.ws.rs.client.Client;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+import static com.github.ingvord.axsis.message.AxsisMoveMessage.AxsisAction.move;
+import static com.github.ingvord.axsis.message.AxsisMoveMessage.AxsisAction.done;
+import static com.github.ingvord.axsis.message.AxsisMoveMessage.AxsisAction.error;
 
 public class AxsisObserver {
+
+    public static final String CHANNEL = "axsis-xes";
+
     static {
         ElasticApmAttacher.attach();
     }
@@ -34,56 +42,109 @@ public class AxsisObserver {
 
         var positionAction = PublishSubject.<AxsisMoveMessage>create();
 
-        var doneAction = PublishSubject.<AxsisDoneMessage>create();
-
-        var errorAction = PublishSubject.<AxsisDoneMessage>create();
-
-        moveAction.subscribe(axsisMoveAction -> {
-            var txn = ElasticApm.startTransaction()
-                    .setName("move");
-            txnRepository.put(axsisMoveAction.id, txn);
-        });
-
-        positionAction.subscribe(axsisPositionAction -> {
-        });
-
-        doneAction.subscribe(doneActionMessage -> {
-            txnRepository.get(doneActionMessage.parentId).end();
-        });
-
-        errorAction.subscribe(errorActionMessage -> {
-            txnRepository.get(errorActionMessage.parentId)
-                    .captureException(new Exception(errorActionMessage.payload.get("error").asText("Unknown error!")));
-        });
+//        var doneAction = PublishSubject.<AxsisDoneMessage>create();
+//
+//        var errorAction = PublishSubject.<AxsisDoneMessage>create();
+//
+//        moveAction.subscribe(axsisMoveAction -> {
+//            var txn = ElasticApm.startTransaction()
+//                    .setName("move");
+//            txnRepository.put(axsisMoveAction.id, txn);
+//        });
+//
+//        positionAction.subscribe(axsisPositionAction -> {
+//        });
+//
+//        doneAction.subscribe(doneActionMessage -> {
+//            txnRepository.get(doneActionMessage.parentId).end();
+//        });
+//
+//        errorAction.subscribe(errorActionMessage -> {
+//            txnRepository.get(errorActionMessage.parentId)
+//                    .captureException(new Exception(errorActionMessage.payload.get("error").asText("Unknown error!")));
+//        });
 
         try(var magix = new SseMagixClient("http://" + System.getenv("MAGIX_HOST"), client)){
             magix.connect();
 
-            magix.observe("axsis-xes")
-                    .subscribeOn(Schedulers.io())
-                    .doOnNext(inboundSseEvent -> System.out.println(inboundSseEvent.readData()))
-                    .blockingSubscribe(inboundSseEvent ->
-                    {
+            var txnStart = magix.observe(CHANNEL)
+                    .flatMap(inboundSseEvent -> {
                         var optionalAxsisMessage = Helpers.tryDecode(inboundSseEvent, AxsisMoveMessage.class);
                         if(optionalAxsisMessage.isPresent()){
-                            var axsisMessage = optionalAxsisMessage.get();
-                            if(axsisMessage.target.equals("axsis") && axsisMessage.payload.action.equals("MOV"))
-                                moveAction.onNext(axsisMessage);
+                            return Observable.just(optionalAxsisMessage.get());
                         }
-                        var optionalAxsisDoneMessage = Helpers.tryDecode(inboundSseEvent, AxsisDoneMessage.class);
-                        if(optionalAxsisDoneMessage.isPresent()){
-                            var axsisMessage = optionalAxsisDoneMessage.get();
-                            if(Optional.ofNullable(axsisMessage.action).orElse("").equals("done"))
-                                doneAction.onNext(axsisMessage);
-                            else if(Optional.ofNullable(axsisMessage.action).orElse("").equals("error"))
-                                errorAction.onNext(axsisMessage);
-                        }
+                        return Observable.error(new IllegalArgumentException("unsupported message"));
+                    })
+                    .filter(axsisMoveMessage -> Objects.nonNull(axsisMoveMessage.action))
+                    .filter(axsisMoveMessage -> axsisMoveMessage.action == move)
+                    .doOnNext(axsisMoveMessage -> System.out.println("Got start"));
 
-                        //TODO report unknown message
+
+            var txnBody = magix.observe(CHANNEL)
+                    .flatMap(inboundSseEvent -> {
+                        var optionalAxsisMessage = Helpers.tryDecode(inboundSseEvent, AxsisMoveMessage.class);
+                        if(optionalAxsisMessage.isPresent()){
+                            return Observable.just(optionalAxsisMessage.get());
+                        }
+                        return Observable.error(new IllegalArgumentException("unsupported message"));
+                    })
+                    .groupBy(AxsisMoveMessage::getParentId)
+                    .filter(group -> group.getKey()!=0L);
+
+
+
+            var txnEnd = magix.observe(CHANNEL)
+                    .doOnSubscribe(disposable -> System.out.println("txnEnd subscribed"))
+                    .doOnNext(inboundSseEvent -> System.out.println("txnEnd"))
+                    .flatMap(inboundSseEvent -> {
+                        var optionalAxsisMessage = Helpers.tryDecode(inboundSseEvent, AxsisMoveMessage.class);
+                        if(optionalAxsisMessage.isPresent()){
+                            return Observable.just(optionalAxsisMessage.get());
+                        }
+                        return Observable.error(new IllegalArgumentException("unsupported message"));
+                    })
+                    .filter(axsisMoveMessage -> Objects.nonNull(axsisMoveMessage.action))
+                    .filter(axsisMoveMessage -> axsisMoveMessage.action == AxsisMoveMessage.AxsisAction.done || axsisMoveMessage.action == AxsisMoveMessage.AxsisAction.error);
+
+
+            magix.observe(CHANNEL)
+                    .subscribeOn(Schedulers.io())
+                    .doOnNext(inboundSseEvent -> System.out.println(inboundSseEvent.readData()))
+                    .flatMap(inboundSseEvent -> {
+                        var optionalAxsisMessage = Helpers.tryDecode(inboundSseEvent, AxsisMoveMessage.class);
+                        if(optionalAxsisMessage.isPresent()){
+                            return Observable.just(optionalAxsisMessage.get());
+                        }
+                        return Observable.error(new IllegalArgumentException("Unsupported message"));
+                    })
+                    .retry()
+                    .buffer(txnStart, v -> txnEnd)
+                    .blockingSubscribe(group ->
+                    {
+                        var txn = ElasticApm.startTransaction();
+                        txn.setName("move");
+                        Observable.fromIterable(group)
+                                .subscribe(message -> {
+                                            switch (message.action) {
+                                                case move:
+                                                    txn.setStartTimestamp(TimeUnit.MICROSECONDS.convert(message.id, TimeUnit.MILLISECONDS));
+                                                    break;
+                                                case done:
+                                                    txn.end(TimeUnit.MICROSECONDS.convert(message.id, TimeUnit.NANOSECONDS));
+                                                    break;
+                                                case error:
+                                                    txn.captureException(new Exception(message.payload.get("error").asText()));
+                                                    break;
+                                            }
+                                        }
+                                );
                     });
         } catch (Exception e){
             throw new RuntimeException(e);
         }
+    }
+
+    public static record AxsisMoveTransaction(long id, List<AxsisMoveMessage> messageChain){
     }
 
 }
--- src/main/java/com/github/ingvord/axsis/message/AxsisMoveMessage.java
+++ src/main/java/com/github/ingvord/axsis/message/AxsisMoveMessage.java
@@ -1,5 +1,7 @@
 package com.github.ingvord.axsis.message;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 import java.util.Map;
 
 public class AxsisMoveMessage {
@@ -7,12 +9,29 @@ public class AxsisMoveMessage {
     public long id;
     public long parentId;
     public String target;
-    public Payload payload;
+    public JsonNode payload;
+    public String user;
+    public AxsisAction action;
 
     public static class Payload {
         public int port;
         public String ip;
         public String action;
         public Map<String, Float> value;
+    }
+
+    public static enum AxsisAction {
+        move,
+        qPOS,
+        done,
+        error
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public long getParentId(){
+        return parentId;
     }
 }
